648c5817e6cd05354be5393c3e7e4a48a576475f
yuszhaqDev <yuszhaq@gmail.com>
2024-08-19 15:16:16 +0100

    intro to factory

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-class Job extends Model
+class Employer extends Model
 {
     use HasFactory;
-    protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
-};
+}
