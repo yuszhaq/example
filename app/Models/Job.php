0bc619b68f1b82b22749790cc626c32a3785fe9d
yuszhaqDev <yuszhaq@gmail.com>
2024-08-19 18:06:30 +0100

    two ket eloquent relation types

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,4 +11,8 @@ class Job extends Model
     use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 };
