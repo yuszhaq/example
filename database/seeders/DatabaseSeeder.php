1fa6d3ff5f7bda48944361a945243f3533403938
yuszhaqDev <yuszhaq@gmail.com>
2024-08-24 18:25:57 +0100

    database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Sakiru',
+            'last_name' => 'Alao',
             'email' => 'test@example.com',
         ]);
+        $this->call(JobSeeder::class);
+        // Job::factory(200)->create();
     }
 }
