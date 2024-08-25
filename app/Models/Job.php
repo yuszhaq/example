32863042c990577c9b7808fab5660eae9347bfbe
yuszhaqDev <yuszhaq@gmail.com>
2024-08-25 18:32:52 +0100

    validation

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,8 +10,8 @@ class Job extends Model
 {
     use HasFactory;
     protected $table = 'job_listings';
-    // protected $fillable = ['title', 'salary', 'employer_id'];
-    protected $guarded = [];
+    protected $fillable = ['title', 'salary', 'employer_id'];
+    // protected $guarded = [];
     public function employer()
     {
         return $this->belongsTo(Employer::class);
