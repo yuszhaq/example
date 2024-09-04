dc324fb66fb82f8d1ad0b80f4b2f9163a49baf27
yuszhaqDev <yuszhaq@gmail.com>
2024-09-04 18:02:34 +0100

    introduction to queue

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,10 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Mail\JobPosted;
 use App\Models\Job;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
@@ -39,7 +35,7 @@ class JobController extends Controller
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
-        Mail::to($job['employer']['user'])->send(
+        Mail::to($job->employer->user)->queue(
             new JobPosted($job)
         );
 
